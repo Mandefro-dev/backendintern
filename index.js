2f6eccb7c3890995fe36512ac22100b9b6a8c88b
Mandefro Endalk <mandefroendalk00@gmail.com>
2024-11-18 09:41:44 -0800

    second

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express=require("express")
 const routes=require("./routes/routes")
 
 const app=express()
-const PORT=process.env.PORT||3000
+const PORT=3000
 app.use("/",routes)
 app.listen(PORT,()=>{
-    console.log(`server is running in port ${PORT}`)
+    console.log(`server is running in PORT ${PORT}`)
 })
